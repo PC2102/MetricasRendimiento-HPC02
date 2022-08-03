1b074809158ce29571a0b5b959f314944ae5dc27
PC2102 <100863015+PC2102@users.noreply.github.com>
2022-08-03 09:33:19 -0500

    Add files via upload

---
+++ Compilacion Separada C++/Body Measure Index/bmi_lib.h
@@ -0,0 +1,23 @@
+/*******************************************************************
+ * Fecha: 27 de Julio de 2022
+ * Autor: Pedro Cardenas
+ * Materia: Introducción HPC
+ * Tema: Lenguaje de Programación C++
+ * Tópico: Aplicación para el Cálculo del Indice de Masa Corporal
+ * *****************************************************************/
+
+#ifndef BMI_LIB_H_INCLUDED
+#define BMI_LIB_H_INCLUDED
+#include <stdio.h>
+#include <iostream>
+#include <math.h>
+
+
+void bienvenida();
+
+float calculoBMI();
+
+void clasificationBMI(float bmiC);
+
+#endif
+
